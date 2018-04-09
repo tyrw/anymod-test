501b678527607aa7e430a41704e2c1c922d65684
Preston Smith (Work) <PrestonII@users.noreply.github.com>
2018-04-09 14:46:33 -0400

    initializing

---
+++ src/anymod.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Demo extends React.Component {
+    componentDidMount () {
+      Anymod.render()
+    }
+    render () {
+      return <div id="anymod-aldlk" data-load="md-mi"></div>
+    }
+  }
+  
+//   ReactDOM.render(
+//     <Demo />,
+//     document.getElementById('demo')
+//   )
